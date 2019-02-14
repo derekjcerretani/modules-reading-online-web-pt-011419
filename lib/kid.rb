1c7064790028c64b85bcae936ba6760af8a2aa2d
Derek Cerretani <derekcerretani@gmail.com>
2019-02-14 19:32:33 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,26 +1,31 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
-=begin
+
+# class Kid
+#   include Dance
+# 
+#   attr_accessor :name
+# 
+#   def initialize(name)
+#     @name = name
+#   end
+# 
+# end
+# 
+# 
+# class Kid
+#   extend MetaDancing
+# end
+
+
 class Kid
-  include Dance
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 
   attr_accessor :name
 
   def initialize(name)
     @name = name
   end
-
-end
-
-
-class Kid
-  extend MetaDancing
-end
-=end
-
-class Kid
-  include Dance
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
 end
