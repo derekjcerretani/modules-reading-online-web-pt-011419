c280059effb086a8ed97ae04f5118805f9d09ea0
Derek Cerretani <derekcerretani@gmail.com>
2019-02-14 18:52:31 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,6 +1,6 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
-
+=begin
 class Dancer
   include Dance
   extend MetaDancing
@@ -12,9 +12,9 @@ class Dancer
   end
 
 end
+=end
 
-=begin
 class Dancer
   extend MetaDancing
 end
-=end
+
